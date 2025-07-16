81e608a0cbe13bcd3d98ce4ff4019236bb837445
= <79530088804@yandex.ru>
2025-07-16 20:37:07 +0500

    Creating a registration

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,9 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 ]
