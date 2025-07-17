b81bc7b3c7c303cd9bf12ff7ef747ea330b5973a
= <79530088804@yandex.ru>
2025-07-17 20:34:20 +0500

    Creating post list

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,9 @@ from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from . import views
 
+
+app_name = 'account'
+
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
--- myblog/urls.py
+++ myblog/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
+    path('account/', include('account.urls', namespace='account')),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
